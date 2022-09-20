55d979917601ed3dee94765483f22cba76a9a847
Riadh mouamnia <riad.sodmg@gmail.com>
2022-09-21 00:46:10 +0100

    Adding styles

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,24 @@
-import Head from "next/head";
-import Image from "next/image";
 import Link from "next/link";
-import Footer from "../components/footer";
-import Nav from "../components/nav";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <div>
-      <h1>hello gangilos</h1>
-      <p>
+      <h1 className={styles.title}>hello gangilos</h1>
+      <p className={styles.text}>
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perferendis
         soluta vero praesentium officiis quo fugiat iste sint ad sequi, libero
         cum blanditiis ea dolorum, aliquam similique. Blanditiis repellat et
         quos!
       </p>
-      <p>
+      <p className={styles.text}>
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perferendis
         soluta vero praesentium officiis quo fugiat iste sint ad sequi, libero
         cum blanditiis ea dolorum, aliquam similique. Blanditiis repellat et
         quos!
       </p>
       <Link href="/gangilos">
-        <a>See Gangilo's Listing</a>
+        <a className={styles.btn}>See Gang's Listing</a>
       </Link>
     </div>
   );
