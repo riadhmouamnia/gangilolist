803d3e394a3715b9663097a6f99142726ffe33dd
Riadh mouamnia <riad.sodmg@gmail.com>
2022-09-21 01:02:26 +0100

    Redirecting users

--- pages/404.js
+++ pages/404.js
@@ -1,6 +1,15 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 const NotFound = () => {
+  const router = useRouter();
+  useEffect(() => {
+    setTimeout(() => {
+      router.push("/");
+    }, 3000);
+  }, []);
+
   return (
     <div className="not-found">
       <h1>Ooooops...</h1>
