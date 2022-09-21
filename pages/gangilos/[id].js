44c5e493e5995cd725799e8a1605876fa4a0d82c
Riadh mouamnia <riad.sodmg@gmail.com>
2022-09-21 11:12:50 +0100

    Fetching single item

--- pages/gangilos/[id].js
+++ pages/gangilos/[id].js
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export const getStaticPaths = async () => {
   const res = await fetch("https://jsonplaceholder.typicode.com/users/");
   const data = await res.json();
@@ -13,11 +15,30 @@ export const getStaticPaths = async () => {
   };
 };
 
-const Details = () => {
+export const getStaticProps = async (context) => {
+  const id = context.params.id;
+  const res = await fetch("https://jsonplaceholder.typicode.com/users/" + id);
+  const data = await res.json();
+
+  return {
+    props: { gang: data },
+  };
+};
+
+const Details = ({ gang }) => {
   return (
-    <div>
-      <h1>Details page</h1>
-    </div>
+    <>
+      <div>
+        <h1>{gang.name}</h1>
+        <p>{gang.email}</p>
+        <p>{gang.website}</p>
+        <p>{gang.address.city}</p>
+      </div>
+      <br />
+      <div>
+        <Link href="/gangilos/">Back to list</Link>
+      </div>
+    </>
   );
 };
 
